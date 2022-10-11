6116d53566e8942cf3022f1beb2d5f5e9a1ff180
FlippErZzDEV <108428973+FlippErZzDEV@users.noreply.github.com>
2022-10-11 12:26:57 +0200

    Initial commit

---
+++ src/main/java/de/swampmc/scheresteinpapier.java
@@ -0,0 +1,17 @@
+package de.swampmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class scheresteinpapier extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
